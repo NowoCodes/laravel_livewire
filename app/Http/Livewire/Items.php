7ca67e4aec86a632c0bebb4dea9a4fc4e351e3b6
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-29 02:07:47 +0100

    AJAX pagination

--- app/Http/Livewire/Items.php
+++ app/Http/Livewire/Items.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Item;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Items extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.items');
+        $items = Item::where('user_id', auth()->id())
+            ->paginate(10);
+
+        return view('livewire.items', compact('items'));
     }
 }
